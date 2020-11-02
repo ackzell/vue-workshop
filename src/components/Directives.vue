bd9c4ef7bb8e4569e46c3ff5df17acf4565444bc
Axel Martínez <ackzell@gmail.com>
2020-11-02 10:21:50 -0600

    Refactoring some of the test, nesting the v-if ones to show a better output.

--- src/components/Directives.vue
+++ src/components/Directives.vue
@@ -19,18 +19,65 @@
       <li>Beatle:</li>
     </ul>
 
-    <!-- ========== Conditionally rendering ========== -->
+    <!-- ========== Conditionally rendering (hiding elements) ========== -->
     <!--
         When the following checkbox is checked,
         we want the answer to be displayed,
         otherwise it should remain hidden.
      -->
+    <hr />
     <label>
       <!-- hint: we already bound the `treasureRevealed` reactive variable on the checkbox -->
       <input type="checkbox" v-model="treasureRevealed" />
       Show <strong>the answer</strong>
     </label>
+    <!-- don't worry about the `data-testid` attribute, we need it for the test -->
     <div data-testid="answer-42">42</div>
+
+    <!-- ========== Conditionally rendering (unmounting elements on the DOM) ========== -->
+    <hr />
+    <label>
+      <!--
+            when choosing Tabs, we want  to render
+            ```
+                <span>
+                    Okay, okay... cool cool cool cool cool
+                </span>
+            ```
+        -->
+      <input type="radio" name="indent" value="Tabs" v-model="indent" />
+      Tabs
+    </label>
+
+    <label>
+      <!--
+            when choosing Spaces, we want  to render
+            ```
+                <span>
+                    No judging here 👀
+                </span>
+            ```
+        -->
+      <input type="radio" name="indent" value="Spaces" v-model="indent" />
+      Spaces
+    </label>
+
+    <br />
+    <br />
+
+    <div data-testid="tabs-vs-spaces-answer">
+      <!--
+        Conditionally add to the DOM the following spans:
+        1. No `indent` value (falsy): "Pick one"
+        2. `indent === "Tabs"` : "Okay, okay... cool cool cool cool cool"
+        3. `indent === "Spaces"` : "No judging here 👀"
+
+        Note that only 1 span should be present at any given time
+     -->
+      <span>Pick one</span>
+      <span>Okay, okay... cool cool cool cool cool</span>
+      <span>No judging here 👀</span>
+    </div>
   </div>
 </template>
 
@@ -44,7 +91,8 @@ export default {
         { name: "George" },
         { name: "Ringo" }
       ],
-      treasureRevealed: false
+      treasureRevealed: false,
+      indent: null
     };
   }
 };
--- tests/unit/directives.spec.js
+++ tests/unit/directives.spec.js
@@ -1,19 +1,61 @@
-import { render, fireEvent, getByText } from "@testing-library/vue";
+import { render, fireEvent } from "@testing-library/vue";
 import Directives from "@/components/Directives.vue";
+import "@testing-library/jest-dom";
 
-xit("should loop through The Beatles names", async () => {
-  const { getByText } = render(Directives);
+/**
+ * Calling render() on every test seems weird, and like beforeEach / beforeAll could be a better approach,
+ * but the lib it self nudges you toward it by design
+ * https://github.com/testing-library/react-testing-library/issues/497#issuecomment-538995327
+ */
 
-  getByText(/John$/i);
-  getByText(/Paul$/i);
-  getByText(/George$/i);
-  getByText(/Ringo$/i);
-});
+describe("Directives test cases", () => {
+  xit("should loop through The Beatles names", async () => {
+    const { getByText } = render(Directives);
+    getByText(/John$/i);
+    getByText(/Paul$/i);
+    getByText(/George$/i);
+    getByText(/Ringo$/i);
+  });
 
-xit("should show the treasure when checked", async () => {
-  const { getByLabelText, getByTestId } = render(Directives);
-  const answerInput = getByLabelText(/show the answer/i);
-  expect(getByTestId("answer-42").style.display).toBe("none");
-  await fireEvent.click(answerInput);
-  expect(getByTestId("answer-42").style.display).toBe("");
+  xit("should show the treasure when checked", async () => {
+    const { getByLabelText, getByTestId } = render(Directives);
+    const answerInput = getByLabelText(/show the answer/i);
+    expect(getByTestId("answer-42").style.display).toBe("none");
+    await fireEvent.click(answerInput);
+    expect(getByTestId("answer-42").style.display).toBe("");
+  });
+
+  describe("should remove and add elements on the DOM", () => {
+    xit('should ONLY show "Pick one" by default', async () => {
+      const { getByText, queryByText } = render(Directives);
+      // Initial state, no selection being made
+      getByText(/Pick one/i);
+      expect(
+        queryByText(/Okay, okay... cool cool cool cool cool/i)
+      ).not.toBeInTheDocument();
+      expect(queryByText(/No judging here/i)).not.toBeInTheDocument();
+    });
+
+    xit('should ONLY show "Okay, okay..." when picking Tabs', async () => {
+      const { getByText, getByLabelText, queryByText } = render(Directives);
+      // Picking tabs
+      const tabsRadio = getByLabelText(/tabs/i);
+      await fireEvent.click(tabsRadio);
+      getByText(/Okay, okay... cool cool cool cool cool/i);
+      expect(queryByText(/Pick one/i)).not.toBeInTheDocument();
+      expect(queryByText(/No judging here/i)).not.toBeInTheDocument();
+    });
+
+    xit('should ONLY show "No judging here" when picking Spaces', async () => {
+      const { getByText, getByLabelText, queryByText } = render(Directives);
+      // Picking tabs
+      const spacesRadio = getByLabelText(/spaces/i);
+      await fireEvent.click(spacesRadio);
+      getByText(/No judging here/i);
+      expect(queryByText(/Pick one/i)).not.toBeInTheDocument();
+      expect(
+        queryByText(/Okay, okay... cool cool cool cool cool/i)
+      ).not.toBeInTheDocument();
+    });
+  });
 });
