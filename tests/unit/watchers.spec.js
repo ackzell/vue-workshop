6717bac19c9b5165299503644d2a2fdaf7869e02
Axel Martínez <ackzell@gmail.com>
2020-11-10 13:07:19 -0600

    Marking watcher tests as skipped.

--- tests/unit/watchers.spec.js
+++ tests/unit/watchers.spec.js
@@ -2,11 +2,11 @@ import { mount } from "@vue/test-utils";
 import Watchers from "@/components/Watchers";
 
 describe("Watchers tests", () => {
-  it("should have a `myFavoriteFlavor` watcher implemented", async () => {
+  xit("should have a `myFavoriteFlavor` watcher implemented", async () => {
     expect(Watchers.watch.myFavoriteFlavor).toBeDefined();
   });
 
-  it("should append a new entry on the logs every time the value of `myFavoriteFlavor` changes", async () => {
+  xit("should append a new entry on the logs every time the value of `myFavoriteFlavor` changes", async () => {
     const wrapper = mount(Watchers);
     await wrapper.find("#fav-b-Chocolate").trigger("click");
     await wrapper.find("#fav-b-Vanilla").trigger("click");
@@ -14,7 +14,7 @@ describe("Watchers tests", () => {
     expect(logs.length).toBe(2);
   });
 
-  it("should display the old value as well as the new value on each entry", async () => {
+  xit("should display the old value as well as the new value on each entry", async () => {
     const wrapper = mount(Watchers);
     await wrapper.find("#fav-b-Chocolate").trigger("click");
     await wrapper.find("#fav-b-Vanilla").trigger("click");
